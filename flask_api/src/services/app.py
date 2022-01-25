f9bfe508f477f9edd025240bc4005695b81f345b
Ruben Castillo <rcastillo@fractal-software.com>
2022-01-25 09:38:34 -0400

    User authentication mechanism (login, logout), User management CRUD (fields: email, password, fullname, photo). and Persistence of data in a Relational database (Postgres is preferred).

--- flask_api/src/services/app.py
+++ flask_api/src/services/app.py
@@ -1,37 +1,46 @@
-from .main import main as main_blueprint
-# from .auth import auth as auth_blueprint
+# from .models import User
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_login import UserMixin
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 
 app.config.from_object("config.Config")
 
 # init SQLAlchemy so we can use it later in our models
-db = SQLAlchemy(app)
+db = SQLAlchemy()
+db.init_app(app)
 
-# # blueprint for auth routes in our app
-# app.register_blueprint(auth_blueprint)
-
-# # blueprint for non-auth parts of app
-# app.register_blueprint(main_blueprint)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
 
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
+
+
+class User(UserMixin, db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100), nullable=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
     photo = db.Column(db.String(100), nullable=True)
-    password = db.Column(db.Integer, nullable=False)
+    password = db.Column(db.String, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
     create_at = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=True,
                            default=datetime.utcnow())
 
-    def __init__(self, email):
+    def __init__(self, email, fullname, password):
         self.email = email
+        self.fullname = fullname
+        self.password = password
 
 
 class Publication(db.Model):
@@ -50,7 +59,3 @@ class Publication(db.Model):
 
     def __init__(self, email):
         self.email = email
-
-
-# if __name__ == '__main__':
-#     app.run()
