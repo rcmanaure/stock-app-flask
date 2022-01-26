6686da16af3fd8a560a66c16e6a1405312df7098
Ruben Castillo <rcastillo@fractal-software.com>
2022-01-26 00:05:29 -0400

    added picture profile

--- flask_api/src/services/forms/forms.py
+++ flask_api/src/services/forms/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from services.app import User
@@ -41,6 +42,8 @@ class UpdateAccountForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
 
     def validate_username(self, username):
--- flask_api/src/services/main.py
+++ flask_api/src/services/main.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, render_template, url_for, flash, redirect, request
 from flask_login import login_required, current_user
 from services.forms.forms import UpdateAccountForm
-from .app import db, User
+from .app import db, app
+import os
+import secrets
+from PIL import Image
 
 main = Blueprint('main', __name__)
 
@@ -43,11 +46,29 @@ def index():
     return render_template('index.html')
 
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(
+        app.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
 @main.route('/profile', methods=['POST', 'GET'])
 @login_required  # Must be log in to see main page.
 def profile():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
