9593f70b743823224831f4089d4efc8320097292
Ruben Castillo <rcmanaure@gmail.com>
2022-03-17 13:45:08 -0400

    working porcentages

--- flask_api/src/services/stock.py
+++ flask_api/src/services/stock.py
@@ -5,11 +5,20 @@ from webargs.flaskparser import use_args
 from marshmallow import fields
 import requests
 import json
-
+import datetime
 # Init the Blueprints of be used in the stock routes.
 stock = Blueprint('stock', __name__)
 
-
+    #   Profit/Loss in percentage
+def profit_loss_pct( actual_price, price, symbol):        
+    profit_loss_prcnt = (actual_price - price)        
+    if profit_loss_prcnt < 0:
+        loss = symbol+" = "+ str(round(profit_loss_prcnt*100/price,2))+"%"
+        return loss
+    else:        
+        profit = symbol + " = " +  str(round(profit_loss_prcnt*100/price,2)) +"%" 
+        return profit
+    
 # Get the data from the https://api.nasdaq.com/api/
 def get_data(symbol:str):   
     res = requests.get("https://api.nasdaq.com/api/quote/{symbol}/info?assetclass=stocks".format(symbol=symbol),headers={
@@ -17,17 +26,17 @@ def get_data(symbol:str):
     
     return res
 
-# # Get the data from the https://api.nasdaq.com/api/
+# # Get the data from the https://api.polygon.io/v2/
 def get_prices(shares): 
     share=[]
+    today = datetime.date.today()
+    today.strftime("%Y-%m-%d")
+    yesterday = today - datetime.timedelta(days=1)    
     for i in shares:       
         symbol = i['symbol']
-        res = requests.get("https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2021-07-22/2021-07-22?adjusted=true&sort=asc&limit=120&apiKey=pBHCmeSoVzoLK3G7R6AO1uMROS5prAf6".format(symbol=symbol),headers={"User-Agent" : "Mozilla/5.0"}).json()    
+        res = requests.get("https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/hour/{yesterday}/{yesterday}?adjusted=true&sort=asc&limit=120&apiKey=pBHCmeSoVzoLK3G7R6AO1uMROS5prAf6".format(symbol=symbol, yesterday= yesterday),headers={"User-Agent" : "Mozilla/5.0"}).json()    
         share.append(res)
-    # print(share)
-    
-    # for i in share:
-    #     print(i['results'])
+   
     return share
 
 @stock.route('/share/', methods=['POST', 'PUT'])
@@ -96,17 +105,55 @@ def list_shares():
     stock_schema = StockResponseSchema(many=True)
     all_stocks = db.session.query(Transaction).all()
     stocks = stock_schema.dumps(all_stocks)               
-    shares = json.loads(stocks)   
-       
-    prueba = get_prices(shares)
+    shares = json.loads(stocks)  
     
-    list_price = []
-    for share in prueba:         
-        list_price.append(share)
-    # print(list_price)
+    # try:
+    #     list_share = get_prices(shares)    
+    #     list_price = []
+    #     for i in list_share:         
+    #         list_price.append(i)     
+    # except Exception:
+    #     return 'Connection Error.(too many request to the api or internet down)'  
+    
+    # ref_prices = []
+    # profit = []
+    # for i in list_price:
+    #     ref_prices.append(i['ticker']+ ' = '+ ' #Lowest price= ' + str(i['results'][0]['l']) + ' #Highest price= ' + str(i['results'][0]['h']) +' #Average price= ' + str(i['results'][0]['vw']))  
+         
+    ref_prices = [{"actual_price": 600}]     
+    share = []
+    money = []    
+    last_price = []
+    profit = []
+    for i in shares:
+        for x in ref_prices:
+            # append to share list the symbol and price and  split the $ sign
+            share.append(i['symbol'] +' : ' + i['last_price'])        
+            splited = str(i['last_price']).split('$')
+            
+            # Convert str to float and  append to last_price list
+            price_splited = float(splited[1])       
+            last_price.append(price_splited)
+            
+            # calculate the profit and loss of shares and append to profit list
+            profit_loss = profit_loss_pct(price_splited, x['actual_price'],i['symbol'])        
+            profit.append(profit_loss)
+            
+            # calculate Current value of the shares holding and append to money list
+            sub_total = price_splited*i['qty']        
+            money.append(i['symbol'] + ' = '+'$ '+ str(sub_total) + '   Qty: ' + str(i['qty'])) 
+            
+             
+   
+    
+    
+    
+ 
+    
+    
     response = make_response(
                 jsonify(
-                    {"data": shares, "prices":list_price}, 
+                    {"data": shares, "value_shares":share, 'current_value_shares_holding': money,"Profit/loss": profit}, 
                 ),
                 200,
             )
@@ -114,8 +161,25 @@ def list_shares():
     return response
     
 
-@stock.route('/stock-prices', methods=['GET','POST'])
+@stock.route('/stock-prices', methods=['GET'])
 # Get the historic price of a stock you bought
-def stock_price(args):
+def stock_price():
     
-    return 'price'
+    stock_schema = StockResponseSchema(many=True)
+    all_stocks = db.session.query(Transaction).all()
+    stocks = stock_schema.dumps(all_stocks)               
+    shares = json.loads(stocks) 
+    list_share = get_prices(shares)
+    
+    list_price = []
+    for share in list_share:         
+        list_price.append(share)       
+  
+    response = make_response(
+                jsonify(
+                    { "prices":list_price}, 
+                ),
+                200,
+            )
+    response.headers["Content-Type"] = "application/json"
+    return response
